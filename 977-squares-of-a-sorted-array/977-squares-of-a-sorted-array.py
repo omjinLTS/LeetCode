3e3356a017cdf43b065a6be7cd34fbbbb72b55d4
omjinLTS <omjin7g@gmail.com>
2022-06-12 13:53:25 +0900

    Time: 407 ms (22.38%), Space: 16.4 MB (17.98%) - LeetHub

--- 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.py
+++ 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.py
@@ -1,3 +1,14 @@
+from collections import deque
 class Solution:
     def sortedSquares(self, nums: List[int]) -> List[int]:
-        return sorted([n**2 for n in nums])
+        q = deque()
+        l, r = 0, len(nums)-1
+        for _ in nums:
+            nL, nR = nums[l]**2, nums[r]**2
+            if nL > nR:
+                q.appendleft(nL)
+                l+=1
+            else:
+                q.appendleft(nR)
+                r-=1
+        return q
